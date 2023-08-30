19bbcc28c0fbad176c678abe23d1b0568125959f
DevExpressExampleBot <fakeEmail>
2023-08-30 17:42:19 +0400

    autogenerate VB

--- VB/DragAndDropRows/Program.vb
+++ VB/DragAndDropRows/Program.vb
@@ -1,27 +1,22 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.LookAndFeel
 
 Namespace DragAndDropRows
-	Friend NotInheritable Class Program
 
-		Private Sub New()
-		End Sub
+    Friend Module Program
 
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-
-			DevExpress.Skins.SkinManager.EnableFormSkins()
-			DevExpress.UserSkins.BonusSkins.Register()
-			UserLookAndFeel.Default.SetSkinStyle("DevExpress Style")
-
-			Application.Run(New Form1())
-		End Sub
-	End Class
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            DevExpress.Skins.SkinManager.EnableFormSkins()
+            DevExpress.UserSkins.BonusSkins.Register()
+            UserLookAndFeel.Default.SetSkinStyle("DevExpress Style")
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
